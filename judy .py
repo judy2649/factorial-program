6b05a9e048b422254f4c921a3b34fddb62ad7777
judy2649 <164991437+judy2649@users.noreply.github.com>
2025-02-21 16:39:11 +0300

    Add files via upload

---
+++ judy .py
@@ -0,0 +1,15 @@
+import time
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 10  
+start_time = time.time()
+result = factorial(number)
+end_time = time.time()
+
+print(f"Factorial of {number} is {result}")
+print(f"Runtime: {end_time - start_time} seconds")
